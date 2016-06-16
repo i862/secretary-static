3678a77738e0f73de72f3ad06e400734dfc79e1e
amenema <men_zhong_xin@163.com>
2016-06-16 21:04:19 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by menzhongxin on 16/6/16.
+ */
+import Vue from 'vue'
+import Router from 'vue-router'
+import App from './components/App.vue'
+Vue.use(Router);
+var router = new Router()
+
+
+router.start(App, '#app')
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+/**
+ * Created by menzhongxin on 16/6/16.
+ */
+var webpack = require('webpack')
+  ,path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname,'./static/lib'),
+    publicPath: "/static/lib/",
+    filename: 'build.js'
+  },
+  module: {
+    // avoid webpack trying to shim process
+    noParse: /es6-promise\.js$/,
+    loaders: [
+      {test: /\.css$/, loader: 'style!css'},
+      {test: /\.vue$/,loader: 'vue'},
+      {test: /\.js$/,exclude: /node_modules|vue\/dist|vue-router\/|vue-loader\/|vue-hot-reload-api\//,loader: 'babel'}
+    ]
+  },
+  babel: {
+    presets: ['es2015'],
+    plugins: ['transform-runtime']
+  },
+  resolve: { fallback: path.join(__dirname, "node_modules") },
+  resolveLoader: { fallback: path.join(__dirname, "node_modules") }
+};
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.plugins = [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.optimize.OccurenceOrderPlugin()
+  ]
+} else {
+  module.exports.devtool = '#source-map'
+}
